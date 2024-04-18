c4ad82e48ed6392f9cc709f66f6e57c8ed15b2ae
Afriza <afrizacho@gmail.com>
2024-04-18 11:42:46 +0700

    add header section

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,10 +1,10 @@
-export default function MenuItem({ title, address, Icon }){
-    return (
-        <Link href={address}>
-            <icon/>
-            <p>
-                {title}
-            </p>
-        </Link>
-    )
+import Link from 'next/link';
+
+export default function MenuItem({ title, address, Icon }) {
+  return (
+    <Link href={address} className='hover:text-amber-500'>
+      <Icon className="text-2xl sm:hidden"/>
+      <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+    </Link>
+  );
 }
